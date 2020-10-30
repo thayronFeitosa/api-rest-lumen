c0ed304cd0b13d09a0a8eddc1afd2e5ca92b7ab4
thayron <thayronf3@gmail.com>
2020-10-29 23:11:31 -0300

    Criando a primeira rota

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+
+
+$router->group(['prefix'=>'/api'], function() use($router){
+    $router->get('/series','SeriesController@index' );
+
+});
